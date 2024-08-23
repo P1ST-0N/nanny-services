e41006863da12ed6278a290187eed0b6842abc82
P1ST-0N <luckylionia@gmail.com>
2024-08-23 21:47:14 +0300

    change App.js & index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
+import App from "./components/App.jsx";
+import "./index.css";
+// import store
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <Provider store={store}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </Provider> */}
+  </StrictMode>
+);
