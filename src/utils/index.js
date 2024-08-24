c2c9a0584586c43b49faf85065102e1f71877535
P1ST-0N <luckylionia@gmail.com>
2024-08-24 19:08:32 +0300

    Create index.js

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+import { handlePending, handleRejected } from "./handleLoading.js";
+import { calcAge } from "./dateFunctions.js";
+import { listSelector } from "./listSelector.js";
+
+export default {
+  handlePending,
+  handleRejected,
+  calcAge,
+  listSelector,
+};
