4e1d8eed0e2b129425217f2e9c7d7c3d2407043e
P1ST-0N <luckylionia@gmail.com>
2024-11-05 12:02:28 +0200

    create AuthForm.jsx & add all depnds

---
+++ src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useDispatch, useSelector } from "react-redux";
+
+import Button from "../Button/Button";
+import CloseButton from "../CloseButton/CloseButton.jsx";
+import Icon from "../Icon/Icon.jsx";
+
+import authOperations from "../../redux/auth/operations.js";
+import authSelectors from "../../redux/auth/selectors.js";
+import schemas from "../../schemas";
+import css from "./AuthForm.module.css";
